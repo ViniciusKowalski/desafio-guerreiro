5c3d9345101f8e226352576c801e84480d06b972
Vinicius Kowalski <vinicius.kowalski@animati.com.br>
2025-04-08 21:10:33 -0300

    Desafio completo

--- index.js
+++ index.js
@@ -36,11 +36,50 @@ Ao final deve se exibir uma mensagem:
   ex: mago atacou usando magia
   guerreiro atacou usando espada
   */
-class nameFighter{
-  constructor(name, age, type){
-    this.name = name
+
+
+//Classe
+class hero{
+  constructor(name, age, type){ // função dentro da classe
+    this.name = name // variavel
     this.age = age
     this.type = type
   }
+  //methodo
+  attack (){
+    let strike;
+
+            if (this.type === "mago"){
+              strike = "magia"
+            }
+            else if (this.type === "guerreiro"){
+              strike = "espada"
+            }
+            else if (this.type === "monge"){
+              strike = "artes marciais"
+            }
+            else if (this.type === "ninja"){
+              strike = "shuriken"
+            }
+            else {
+              strike = "não houve ataque"
+            }
+        switch(this.type){
+          case "mago":
+              console.log(`O ${this.name}, atacou usando ${strike}`)
+          break;
+          case "guerreiro":
+            console.log(`O ${this.name}, atacou usando ${strike}`)
+          break;
+          case "monge":
+            console.log(`O ${this.name}, atacou usando ${strike}`)
+          break;
+          case "ninja":
+            console.log(`O ${this.name}, atacou usando ${strike}`)
+          break;
+        }
+  }  
 }
+const fighter = new hero('Aragorn', 32, "guerreiro")
+fighter.attack()
 
